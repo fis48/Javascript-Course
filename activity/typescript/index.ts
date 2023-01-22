6a4c2e3079f7dfafa625afb629cdc9612670c047
fis48 <info@fidelsilva.com>
2023-01-22 18:08:26 -0500

    entrega typescript

--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,114 @@
 // Ejercicio 1
+interface Person {
+  name: string;
+  age: number;
+  profession: string;
+}
+const response = (person:Person): string | number[] => {
+  return Object.values(person);
+}
+const responseGen = <T>(person:Person): Array<T> => {
+  return Object.values(person);
+}
+const someone:Person = {
+  name: "Juan Silva",
+  age: 23,
+  profession: "Software Engineer"
+}
+
+console.log();
+console.log("::: EJERCICIO 1 :::");
+const someoneKeysGen = response(someone);
+console.log("someone gen:", someoneKeysGen);
+const someoneValues = response(someone);
+console.log("someone:", someoneValues);
 
 // Ejercicio 2
+const sumOrConcatenate = <S, N>(param1:S, param2:N) => {
+  let result: string | number;
+  if (typeof param1 === "number" && typeof param2 === "number" ) {
+    result = param1 + param2;
+  }
+  else if (typeof param1 === "string" && typeof param2 === "string" ) {
+    result = `${param1} - ${param2}`;
+  }
+  else {
+    result = `${param1} : ${param2}`;
+  }
+  return result;
+}
+
+console.log();
+console.log("::: EJERCICIO 2 :::");
+const twoStrings = sumOrConcatenate("1", "2");
+const twoNumbers = sumOrConcatenate(1, 2);
+const mixed = sumOrConcatenate("1", 2);
+const otherMix = sumOrConcatenate(1, "2");
+
+console.log("twoStrings:", twoStrings);
+console.log("twoNumbers:", twoNumbers);
+console.log("mixed:", mixed);
+console.log("otherMix:", otherMix);
 
 // Ejercicio 3
+interface Car {
+  tires: number;
+  turnOnEngine: () => void;
+  pressPedal: () => void;
+}
+interface Motorcycle {
+  tires: number;
+  turnOnEngine: () => void;
+  openThrottle: () => void;
+}
+const isCar = (vehicle: Car | Motorcycle): vehicle is Car => {
+  return (vehicle as Car).pressPedal !== undefined; 
+}
+const responseVehicle = (vehicle: Car | Motorcycle) => {
+  vehicle.turnOnEngine();
+  if (isCar(vehicle)) {
+    vehicle.pressPedal();
+  }
+  else {
+    vehicle.openThrottle();
+  }
+}
+console.log();
+console.log("::: EJERCICIO 3 :::");
+const car:Car = {
+  tires: 4,
+  turnOnEngine() {
+    console.log("Engine turned on.");
+  },
+  pressPedal() {
+    console.log("Pedal pressed.");
+  }
+}
+const motorcycle:Motorcycle = {
+  tires: 2,
+  turnOnEngine() {
+    console.log("Engine turned on.")
+  },
+  openThrottle() {
+    console.log("Throttle opened.");
+  },
+}
+responseVehicle(car);
+responseVehicle(motorcycle);
 
 // Ejercicio 4
+const genericFunc = <T>(items:Array<T>) => {
+  const result = [...items];
+  result.shift();
+  return result;
+}
+
+const strings:string[] = ["a", "b", "c"];
+const numbers: number[] = [1, 2, 3];
+const resultStrings = genericFunc(strings);
+const resultNumbers = genericFunc(numbers);
+
+console.log();
+console.log("::: EJERCICIO 4 :::");
+console.log("Strings:", resultStrings);
+console.log("Numbers:", resultNumbers);
